4adc236194ccfcb0b1441e67ecbb6c42af3de88f
jadchayan <adchayan.jeyakumar@etu.cyu.fr>
2025-04-10 11:50:41 +0200

    Create structures.h

---
+++ structures.h
@@ -0,0 +1,50 @@
+typedef enum {
+    boost_att = 1,
+    boost_def,
+    boost_vitesse,
+    soin_progressif
+} EffetStat;
+
+typedef enum {
+    poison = 1,
+    stun,
+    silence,
+    gel
+} EffetStatut;
+
+typedef struct {
+    EffetStat effet_stat;
+    EffetStatut effet_statut;
+    int duree;
+    float valeur;
+} EffetActif;
+
+typedef struct {
+    char *nom;                     
+    float valeur;                 
+    char description[1000];
+    int nbtactifs;                
+    int nbtrechargement;          
+    EffetStat effet_stat;         
+    EffetStatut effet_statut;     
+} Techniquespe;
+
+typedef struct {
+    float pvcourant;
+    float pvmax;
+    float att;
+    float def;
+    float agilite;
+    float vitesse;
+    EffetActif effets[3]; 
+    Techniquespé tech;
+    float jauge;
+    int statut; //mort ou vivant
+    char nom[100];
+} Champ;
+
+typedef struct {
+    char *nom;
+    Champ membres[3];
+} Equipe;
+
