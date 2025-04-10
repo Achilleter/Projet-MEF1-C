2ae058816c05f2ba7af7dab24874a0255735e592
Achille <achille.therin@etu.cyu.fr>
2025-04-10 12:14:16 +0200

    Rajouts debuts fonctions statuts et techspé

--- attaque.c
+++ attaque.c
@@ -9,6 +9,29 @@ float attaque(Champ champatt, Champ champdef){
     return champdef.pvcourant;
 }
 
-Champ techspé(Champ champatt, Champ champdef){
-    
+Champ techspé(Champ champatt, Champ champdef, int* nbtactifs, int* nbtrechargment){
+    if(champatt.tech.nbtactifs>1){
+        *nbtactifs=champatt.tech.nbtactifs;
+    }
+    if(champatt.tech.nbtrechargement>1){
+        *nbtrechargment=champatt.tech.nbtrechargement;
+    }
+    if(champatt.effet_statut!=0){
+        champdef.statut=champatt.tech.effet_statut;
+    }
+    if(champatt.effet_stat!=0){
+        champdef.stat=champatt.tech.effet_stat;
+    }
+}
+
+void appeffet_Statut(Champ champ){
+        switch(champ.statut){
+            case 1:
+                champ.pvcourant-=champ.pvmax*0.05;
+                break;
+            case 3:
+                champ.vitesse-=champ.vitesse*0.5;
+                break;
+            default:
+                break;
 }
