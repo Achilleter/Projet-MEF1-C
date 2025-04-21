d9cb69dbb4882f97d56a9e35d7257024b2cf8a89
RemiSath <remisaththiriyan@gmail.com>
2025-04-21 13:06:49 +0200

    fichier

--- utilitaire.c
+++ utilitaire.c
@@ -3,29 +3,35 @@
 Champ getchamp(char nom[20],Champ champ){
     FILE* fp=fopen("combattants.txt", "r");
     char string[100];
-    while(fgets(string, 20, fp)!=NULL){
-        if(strcmp(string, nom) == 0){
-            strcpy(champ.nom, string);
-            fgets(string, 20, fp);
-            champ.pvmax=atoi(string);
-            fgets(string, 20, fp);
-            champ.att=atoi(string);
-            fgets(string, 20, fp);
-            champ.def=atoi(string);
-            fgets(string, 20, fp);
-            champ.agilite=atoi(string);
-            fgets(string, 20, fp);
-            champ.vitesse=atoi(string);
-            fgets(string, 20, fp);
-            champ.tech.nom=string;
-            fgets(string, 20, fp);
-            champ.tech.nom=string;
-            champ.pvcourant=champ.pvmax;
-            champ.jauge=0;
-            champ.nbeffets=0;
-            champ.statut=1; // vivant
+    if(fp!= NULL){
+        while(fgets(string, 99, fp)!=NULL){
+            if(strcmp(string, nom) == 0){
+                strcpy(champ.nom, string);
+                fgets(string, 99, fp);
+                champ.pvmax=atoi(string);
+                fgets(string, 99, fp);
+                champ.att=atoi(string);
+                fgets(string, 99, fp);
+                champ.def=atoi(string);
+                fgets(string, 99, fp);
+                champ.agilite=atoi(string);
+                fgets(string, 99, fp);
+                champ.vitesse=atoi(string);
+                fgets(string, 99, fp);
+                champ.tech.nom=string;
+                fgets(string, 99, fp);
+                champ.tech.nom=string;
+                champ.pvcourant=champ.pvmax;
+                champ.jauge=0;
+                champ.nbeffets=0;
+                champ.statut=1; // vivant
+            }
         }
+        fclose(fp);
     }
-    fclose(fp);
+    else{
+        printf("Erreur d'ouverture du fichier\n");
+        exit(1);
+    }
     return champ;
 }
