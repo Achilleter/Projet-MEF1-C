c398a57684c49506855550c5b5e9607419b8b434
RemiSath <remisaththiriyan@gmail.com>
2025-05-12 15:08:30 +0200

    Revert " à modifier"
    This reverts commit 32b8ad83be113939d4501f684f13d0a64383f87f.

---
+++ champ_select.c
@@ -0,0 +1,91 @@
+#include "principal.h"
+
+void selection(Champ tous[], Equipe* e1, Equipe* e2){  
+    if (e1 == NULL || e2 == NULL){ // Vérification des pointeurs e1 et e2
+        printf("Erreur : pointeur nul");
+        exit(10);
+    }
+    int deja_pris[10]={0}; // Tableau pour savoir quels champions ont déjà été pris
+    for (int i=0; i<3; i++) { // Debut de la boucle pour choisir les champions
+        int choix;
+        //debut du choix pour le joueur 1
+        printf("%s, choisissez votre combattant %d :\n\n", e1->nom, i+1);
+        afficherEquipeChamp(tous, deja_pris); // Appelle la fonction pour afficher les champions disponibles
+        int verif;
+        do { // Verification de la saisie
+            choix=0;
+            printf("\nVotre choix: ");
+            verif=scanf("%d", &choix);
+            vide_buffer();
+            if(choix<1 || choix>10){
+                printf("Index invalide\n");
+            }
+            else if(deja_pris[choix-1]==1){ // Si le champion a déjà été pris il ne peut pas être choisi
+                printf("Ce champion a deja ete pris !\n");
+            }
+        }while (choix<1 || choix>10 || deja_pris[choix-1]==1 || verif!=1);
+        e1->membres[i]=tous[choix-1];
+        deja_pris[choix-1]=1; // On marque le champion comme pris
+        printf("%s a selectionne %s\n\n", e1->nom, e1->membres[i].nom);
+        //debut du choix pour le joueur 2
+        printf("%s, choisissez votre combattant %d:\n\n", e2->nom, i+1);
+        afficherEquipeChamp(tous, deja_pris);
+        do { // Verification de la saisie
+            choix=0;
+            printf("\nVotre choix: ");
+            verif=scanf("%d", &choix);
+            vide_buffer();
+            if(choix<1 || choix>10){
+                printf("Index invalide\n");
+            }
+            else  if(deja_pris[choix-1]==1){ // Si le champion a déjà été pris il ne peut pas être choisi
+                printf("Ce champion a deja ete pris !\n");
+            }
+        } while (choix<1 || choix>10 || deja_pris[choix-1]==1 || verif!=1);
+        e2->membres[i]=tous[choix-1];
+        deja_pris[choix-1]=1; // On marque le champion comme pris
+        printf("%s a selectionne %s\n\n", e2->nom, e2->membres[i].nom);
+    }
+    // Fin de la boucle pour choisir les champions
+    e1->nbchampvivant=3; // Initialisation du nombre de champions vivants pour chaque équipe
+    e2->nbchampvivant=3;
+    printf("\n\n");
+}
+
+void selectionia(Champ tous[], Equipe* e1, Equipe* e2){
+    int deja_pris[10]={0}; // Tableau pour savoir quels champions ont déjà été pris
+    for (int i=0; i<3; i++) { // Debut de la boucle pour choisir les champions
+        int choix;
+        //debut du choix pour le joueur 1
+        printf("%s, choisissez votre combattant %d :\n\n", e1->nom, i+1);
+        afficherEquipeChamp(tous, deja_pris); // Appelle la fonction pour afficher les champions disponibles
+        int verif;
+        do { // Verification de la saisie
+            choix=0;
+            printf("\nVotre choix: ");
+            verif=scanf("%d", &choix);
+            vide_buffer();
+            if(choix<1 || choix>10){
+                printf("Index invalide\n");
+            }
+            else if(deja_pris[choix-1]==1){ // Si le champion a déjà été pris il ne peut pas être choisi
+                printf("Ce champion a deja ete pris !\n");
+            }
+        }while (choix<1 || choix>10 || deja_pris[choix-1]==1 || verif!=1);
+        e1->membres[i]=tous[choix-1];
+        deja_pris[choix-1]=1; // On marque le champion comme pris
+        printf("%s a selectionne %s\n\n", e1->nom, e1->membres[i].nom);
+        //debut du choix pour l'IA
+        do{
+            choix=rand()%10+1;
+        }
+        while(deja_pris[choix-1]==1); // On choisit un champion au hasard qui n'a pas encore été pris
+        e2->membres[i]=tous[choix-1];
+        deja_pris[choix-1]=1; // On marque le champion comme pris
+        printf("%s a selectionne %s\n\n", e2->nom, e2->membres[i].nom);
+    }
+    // Fin de la boucle pour choisir les champions
+    e1->nbchampvivant=3; // Initialisation du nombre de champions vivants pour chaque équipe
+    e2->nbchampvivant=3;
+    printf("\n\n");
+}
---
+++ utilitaire.c
@@ -0,0 +1,66 @@
+#include "principal.h"
+
+void getchamp(Champ tab[]){
+    // recupere les champions depuis le fichier champions.txt et les stocke dans le tableau tab
+    FILE* fp=fopen("champions.txt", "r"); // Ouvre le fichier champions.txt en mode lecture
+    if(fp!= NULL){
+        char string[99]; // tableau de char pour stocker les lignes du fichier
+        for(int i=0; i<10; i++){
+            // on recupere les informations de chaque champion lignes par lignes
+            fgets(string, 99, fp); // lit une ligne du fichier
+            strcpy(tab[i].nom, string);
+            fgets(string, 99, fp);
+            tab[i].pvmax=atoi(string);
+            fgets(string, 99, fp);
+            tab[i].att=atoi(string);
+            fgets(string, 99, fp);
+            tab[i].def=atoi(string);
+            fgets(string, 99, fp);
+            tab[i].agilite=atoi(string);
+            fgets(string, 99, fp);
+            tab[i].vitesse=atoi(string);
+            fgets(string, 99, fp);
+            tab[i].jaugemax=atoi(string);
+            fgets(string, 99, fp);
+            strcpy(tab[i].tech.nom, string);
+            tab[i].pvcourant=tab[i].pvmax;
+            tab[i].jaugeactuelle=0;
+            tab[i].nbeffets=0;
+            tab[i].tech.nbtactifs=0;
+            for(int j=0; j<10; j++){
+                tab[i].effets[j].duree=0;
+                tab[i].effets[j].effet_statut=0;
+            }
+            tab[i].statut=1; // initialise à vivant
+        }
+        fclose(fp);
+    }
+    else{
+        // si le fichier n'existe pas ou ne peut pas être ouvert, on affiche un message d'erreur et on quitte le programme
+        printf("Erreur d'ouverture du fichier\n");
+        exit(1);
+    }
+    for (int i = 0; i < 10; i++) {
+        // Nettoyage des noms de champions pour enlever les retours à la ligne
+        nettoyerNom(tab[i].nom);
+        nettoyerNom(tab[i].tech.nom);
+    }
+}
+
+void vide_buffer(){
+    // vide le buffer d'entrée pour éviter les erreurs de lecture
+    while (getchar()!='\n'){
+    }
+}
+
+void nettoyerNom(char* nom) {
+    // Enlève le retour à la ligne et les espaces à la fin du nom
+    int len = strlen(nom);
+    if (len > 0 && nom[len - 1] == '\n') {
+        nom[len - 1] = '\0';
+    }
+    while (len > 0 && nom[len - 1] == ' ') {
+        nom[len - 1] = '\0';
+        len--;
+    }
+}
